068765db541b6ff0cddae45a4a2a3c973489f6f7
daoready <info@daoready.com>
2018-04-20 18:35:13 +0300

    changes from bulk-rpc branch

---
+++ rpc/src/v1/traits/bulk.rs
@@ -0,0 +1,37 @@
+// Copyright 2015-2017 Parity Technologies (UK) Ltd.
+// This file is part of Parity.
+
+// Parity is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Eth rpc interface.
+use jsonrpc_core::{BoxFuture, Error};
+use jsonrpc_macros::Trailing;
+
+use v1::types::{RichBlock, BlockNumber, Bytes, CallRequest, Filter, FilterChanges, Index, BlockWithTransactions};
+use v1::types::{Log, Receipt, SyncStatus, Transaction, Work};
+use v1::types::{H64, H160, H256, U256};
+
+build_rpc_trait! {
+	/// Bulk rpc interface.
+	pub trait Bulk {
+		type Metadata;
+
+		/// Returns block with given number.
+		#[rpc(name = "bulk_getBlockByNumber")]
+		fn block_by_number(&self, BlockNumber) -> BoxFuture<Option<BlockWithTransactions>, Error>;
+
+
+	}
+}
+
